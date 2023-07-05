5ebd62444f44a39769bcf594c2fe10ecedcbe63d
DirkdToit <dirk@brightedge.co.za>
2023-07-05 18:58:23 +0200

    Added hooks folder and useResults.js

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from 'react-native';
 import SearchBar from "../Components/SearchBar";
 import yelp from "../api/yelp";
@@ -6,25 +6,42 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
-    const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: searchTerm,
-                location: 'san jose'
-            }
-        });
-        setResults(response.data.businesses);
-    }
+    const searchApi = async (searchNewTerm) => {
+        console.log('Searched API');
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchNewTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+            setErrorMessage("");
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    }   
+
+    //Never call an api in here 
+    //Api gets called > state gets updated > Rerender component > api gets called => Infinite loop
+    //That is why useEffect is used, and not just "searchApi('Pasta')"
+
+    useEffect(() => {
+        searchApi('Pasta');
+    }, []
+    );
 
     return (
         <View style={styles.background}>
             <SearchBar 
                 searchTerm={searchTerm} 
                 onSearchTermChange={setSearchTerm} 
-                onSearhTermSubmit={searchApi}
+                onSearhTermSubmit={() => {searchApi(searchTerm)}}
             />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
     );
