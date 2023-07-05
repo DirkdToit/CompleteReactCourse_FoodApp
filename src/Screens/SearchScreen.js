0128e9fd03eb967d5101199f006053aec7663848
DirkdToit <dirk@brightedge.co.za>
2023-07-05 20:37:45 +0200

    Added resultslist and displayed names of restaurants

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,38 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text } from 'react-native';
 import SearchBar from "../Components/SearchBar";
 import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
+import ResultsList from "../Components/resultsList";
 
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState("");
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState("");
+    const [searchApi, results, errorMessage] = useResults();
 
-    const searchApi = async (searchNewTerm) => {
-        console.log('Searched API');
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchNewTerm,
-                    location: 'san jose'
-                }
-            });
-            setResults(response.data.businesses);
-            setErrorMessage("");
-        } catch (err) {
-            setErrorMessage('Something went wrong');
-        }
-    }   
-
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
+    
     //Never call an api in here 
     //Api gets called > state gets updated > Rerender component > api gets called => Infinite loop
     //That is why useEffect is used, and not just "searchApi('Pasta')"
-
-    useEffect(() => {
-        searchApi('Pasta');
-    }, []
-    );
 
     return (
         <View style={styles.background}>
@@ -43,6 +28,9 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender!"/>
         </View>
     );
 };
--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,16 +1,38 @@
-import React from "react";
-import { View, StyleSheet, Text } from 'react-native';
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
 
-const CHANGE = () => {
-    return (
-        <View>
-            <Text>This is the new screen</Text>
-        </View>
+export default () => {
+
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const searchApi = async (searchNewTerm) => {
+        console.log('Searched API');
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchNewTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+            setErrorMessage("");
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    }   
+
+    //Never call an api in here 
+    //Api gets called > state gets updated > Rerender component > api gets called => Infinite loop
+    //That is why useEffect is used, and not just "searchApi('Pasta')"
+
+    useEffect(() => {
+        searchApi('Pasta');
+    }, []
     );
+
+    return [searchApi, results, errorMessage];
+
 };
-
-const styles = StyleSheet.create({ 
-});
-
-export default CHANGE;
  
